3a3f765420f034913652560a0f2d83b1c20c8310
Chelsie Conrad <chelsiechey@gmail.com>
2019-09-25 19:23:58 -0600

    added navbar and links to each page

---
+++ client/src/components/blog/Blog.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Header } from 'semantic-ui-react';
+
+class Blog extends Component {
+  render() {
+    return (
+      <>
+        <Header>Blog</Header>
+      </>
+    )
+  }
+}
+
+export default Blog;
